d200bd74508c116556558caaf938482dda975dcc
gbicyt <xboxm1300@gmail.com>
2022-12-09 06:57:18 +0700

    xyz

---
+++ config.js
@@ -0,0 +1,410 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.thum = fs.readFileSync("./media/zaki.jpg")
+
+global.autoReadAll = true
+// Ganti Sewajarnya
+global.owner = ['6282125170315']
+global.pengguna = 'XyzeeBotzz'
+global.premium = []
+global.author = 'XyzeeBotzz'
+global.pemilik = ['6282125170315'] 
+global.ownernomer = '6282125170315'
+global.ownername = 'XyzeeXditz'
+global.ownerName = '© XyzeeStore'
+global.botname = 'XyzeeBotzz'
+global.Prefix = 'Multi Prefix'
+global.meki = '6282125170315'
+global.footer = 'X y z e e'
+global.footer1 = 'Jangan Spam Bot.'
+global.footer2 = 'Jika Menemukan Bug Lapor Ke Owner.'
+global.yt = 'None'
+global.ig = 'None'
+global.gc = 'None'
+global.dana = '082125170315'
+global.gopay = '082125170315'
+global.wame = 'wa.me/6282125170315'
+global.email = 'xyzeestore70@gmail.com'
+global.region = 'Indonesia'
+global.link = 'nekopoi.care'
+global.sc = 'https://bit.ly/SubscribeFLOB'
+global.packname = 'Xyzee' 
+global.author = 'Studios'
+global.sessionName = 'kizakixd'
+global.prefa = ['#','!','/','']
+global.sp = '❑ ' 
+global.mess = {
+    success: 'Done Your Request',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    error: 'Fitur sedang error!',
+        endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+    wrongFormat: 'Perintah Salah!!\nSertakan Link setelah Command..',
+    example1: 'Welcome @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @subject (Group Name)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)'
+,
+    example2: 'Good Bye @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @subject (Group Name)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)'    
+    
+}
+    global.limitawal = {
+    premium: "1000",
+    free: 500,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./media/zaki.jpg')
+global.fakeImg = fs.readFileSync('./media/fake.jpg')
+
+//Ambil dr chika
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+//Menu
+global.ccomannya = ` 
+「 *Main Menu* 」
+ ❐ script
+ ❐ speedtest
+ ❐ ping
+ ❐ owner
+ ❐ menu
+ ❐ delete
+ ❐ infochat
+ ❐ quoted
+ ❐ listpc
+ ❐ listonline
+ ❐ react [emoji] 
+  
+  「 *Group Menu* 」
+ ❐ linkgroup
+ ❐ ephemeral [option]
+ ❐ setppgc [image]
+ ❐ setname [text]
+ ❐ setdesc [text]
+ ❐ group [option]
+ ❐ editinfo [option]
+ ❐ add @user
+ ❐ kick @user
+ ❐ hidetag [text]
+ ❐ tagall [text]
+ ❐ antilink [on/off]
+ ❐ mute [on/off]
+ ❐ promote @user
+ ❐ demote @user
+ ❐ vote [text]
+ ❐ devote
+ ❐ upvote
+ ❐ cekvote
+ ❐ hapusvote
+
+  「 *Owner Menu* 」
+ ❐ session  
+ ❐ chat [option]
+ ❐ join [link]
+ ❐ leave
+ ❐ block @user
+ ❐ setbiobot [text] 
+ ❐ unblock @user
+ ❐ bcgroup [text]
+ ❐ bcall [text]
+ ❐ setppbot [image]
+ ❐ setexif
+  
+  「 *Rpg Menu* 」
+ ❐ berburu
+ ❐ mancing
+ ❐ menambang
+ ❐ mining
+ ❐ heal
+ ❐ profile
+ ❐ inventory
+ ❐ leaderboard
+ ❐ buy
+ ❐ sell
+
+  「 *Downloader Menu* 」
+ ❐ tiktok [url]
+ ❐ instagram [url]
+ ❐ twitter [url]
+ ❐ facebook [url]
+ ❐ igstory [query]
+ ❐ umma [url]
+ ❐ mediafire [url]
+ ❐ gitclone [url]
+ 
+  「 *Text Pro Menu* 」
+ ❐ textmaker glicth [Text]
+ ❐ textmaker glow [Text]
+ ❐ flasmurf [Text]
+ ❐ flarun [Text]
+ ❐ fluming [Text]
+ ❐ flaming [Text]
+ ❐ lava [Text]
+ ❐ neonlight [Text]
+ ❐ fabric [Text]
+ ❐ glue [Text]
+ ❐ luxury [Text]
+ ❐ underwater [Text]
+ ❐ harrypotter [Text]
+ ❐ steel [Text]
+ ❐ metallic [Text]
+ ❐ graffiti [Text]
+ ❐ pencil [Text]
+ ❐ sand [Text]
+ ❐ magma [Text]
+ ❐ joker [Text]
+ ❐ sky [Text]
+ ❐ matrix [Text]
+ ❐ neon [Text]
+ ❐ blackpink [Text]
+ ❐ thunder [Text]
+ ❐ whitebear [Text]
+ ❐ hoorror [Text]
+ ❐ 8bit [Text]
+ ❐ horror [Text]
+ ❐ retro [Text]
+ ❐ pornhub [Text]
+ ❐ juice [Text]
+ ❐ batman [Text]
+ ❐ multicolor [Text]
+ ❐ collwall [Text]
+ ❐ cool [Text]
+ ❐ wonderful [Text]
+ ❐ sketch [Text]
+ ❐ skeleton [Text]
+ ❐ marvel [Text]
+ ❐ foggy [Text]
+ ❐ writing [Text]
+ ❐ halloween [Text]
+ ❐ watercolor [Text]
+ ❐ classic [Text]
+ ❐ rainbow [Text]
+ ❐ sci_fi [Text]
+ ❐ christmas [Text]
+ ❐ ancient [Text]
+ ❐ toxic [Text]
+
+  「 *Stalker Menu* 」
+ ❐ ghstalk [query]
+ ❐ igstalk [query]
+ ❐ ffstalk [query]
+ ❐ supersusstalk [query]
+
+  「 *Internet Menu* 」
+ ❐ ip [query]
+ ❐ iploc [query]
+ ❐ ipinfo [query]
+ ❐ ssweb [query]
+  
+  「 *Search Menu* 」
+ ❐ play [query]
+ ❐ yts [query]
+ ❐ google [query]
+ ❐ gimage [query]
+ ❐ pinterest [query]
+ ❐ wallpaper [query]
+ ❐ wikimedia [query]
+ ❐ ytsearch [query]
+ ❐ ringtone [query]
+ ❐ stalk [option] [query]
+ ❐ webtoons [query]
+ ❐ drakor [query]
+ ❐ anime [query]
+ ❐ character [query]
+ ❐ manga [query]
+
+  「 *Random Menu* 」
+ ❐ coffe
+ ❐ quotesanime
+ ❐ couple
+ ❐ anime
+ ❐ waifu (nsfw)
+ ❐ husbu (nsfw)
+ ❐ neko (nsfw)
+ ❐ shinobu (nsfw)
+ ❐ waifus (nsfw)
+ ❐ nekos (nsfw)
+ ❐ trap (nsfw)
+ ❐ blowjob (nsfw)
+
+  「 *Random Anime Menu* 」
+ ❐ loli
+ ❐ neko
+ ❐ waifu
+ ❐ shinobu
+ ❐ megumin
+ ❐ bully
+ ❐ cuddle
+ ❐ cry
+ ❐ hug
+ ❐ awoo
+ ❐ kiss
+ ❐ lick
+ ❐ pat
+ ❐ smug
+ ❐ bonk
+ ❐ yeet
+ ❐ blush
+ ❐ smile
+ ❐ wave
+ ❐ highfive
+ ❐ handhold
+ ❐ nom
+ ❐ bite
+ ❐ glomp
+ ❐ slap
+ ❐ kill
+ ❐ happy
+ ❐ wink
+ ❐ poke
+ ❐ dance
+ ❐ cringe
+
+  「 *Fun Menu* 」
+ ❐ conves [628xxx|nama|pesan]  
+ ❐ bagaimanakah
+ ❐ kapankah
+ ❐ apakah
+ ❐ bisakah
+ ❐ rate
+ ❐ wangy
+ ❐ gantengcek
+ ❐ cekganteng
+ ❐ cantikcek
+ ❐ cekcantik
+ ❐ sangecek
+ ❐ ceksange
+ ❐ cekme
+ ❐ gaycek
+ ❐ cekgay
+ ❐ lesbicek
+ ❐ halah
+ ❐ hilih
+ ❐ huluh
+ ❐ heleh
+ ❐ holoh
+ ❐ jadian
+ ❐ jodohku
+ ❐ delttt
+ ❐ tictactoe
+ ❐ family100
+ ❐ tebak [option]
+ ❐ math [mode]
+ ❐ suitpvp [@tag]
+
+  「 *Primbon Menu* 」
+ ❐ nomorhoki
+ ❐ artimimpi
+ ❐ artinama
+ ❐ ramaljodoh
+ ❐ ramaljodohbali
+ ❐ suamiistri
+ ❐ ramalcinta
+ ❐ cocoknama
+ ❐ pasangan
+ ❐ jadiannikah
+ ❐ sifatusaha
+ ❐ rezeki
+ ❐ pekerjaan
+ ❐ nasib
+ ❐ penyakit
+ ❐ tarot
+ ❐ fengshui
+ ❐ haribaik
+ ❐ harisangar
+ ❐ harisial
+ ❐ nagahari
+ ❐ arahrezeki
+ ❐ peruntungan
+ ❐ weton
+ ❐ karakter
+ ❐ keberuntungan
+ ❐ memancing
+ ❐ masasubur
+ ❐ zodiak
+ ❐ shio
+
+  「 *Convert Menu* 」
+ ❐ toimage
+ ❐ removebg
+ ❐ sticker
+ ❐ emojimix
+ ❐ tovideo
+ ❐ togif
+ ❐ tourl
+ ❐ tovn
+ ❐ tomp3
+ ❐ toaudio
+ ❐ ebinary
+ ❐ dbinary
+ ❐ styletext
+ ❐ smeme
+
+  「 *Database Menu* 」
+ ❐ setcmd
+ ❐ listcmd
+ ❐ delcmd
+ ❐ lockcmd
+ ❐ addmsg
+ ❐ listmsg
+ ❐ getmsg
+ ❐ delmsg
+
+ 「 *Bug Menu* 」
+ ❐ virtext1
+ ❐ virtext2
+ 
+  「 *Anonymous Menu* 」
+ ❐ anonymous
+ ❐ start
+ ❐ next
+ ❐ keluar
+ ❐ sendkontak
+
+  「 *Islamic Menu* 」
+ ❐ iqra
+ ❐ hadist
+ ❐ alquran
+ ❐ juzamma
+ ❐ tafsirsurah
+
+  「 *Voice Changer* 」
+ ❐ bass
+ ❐ blown
+ ❐ deep
+ ❐ earrape
+ ❐ fast
+ ❐ fat
+ ❐ nightcore
+ ❐ reverse
+ ❐ robot
+ ❐ slow
+ ❐ tupai
+ 
+「 *Thanks To* 」
+ ❐ Marpel Botz
+ ❐ Xyzee Botzz
+`
+global.sewanya =
+'  *• List Sewabot :*\n   _• 1 Minggu : 5K_\n   _• 1 Bulan : 10K_\n   _• Permanent : 50K_'
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
